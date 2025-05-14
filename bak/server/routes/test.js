2bb36cf419d6f5433a0ab0dfda80394a7995e501
keshevplus <pluskeshev@gmail.com>
2025-05-14 10:47:23 +0300

    WIP: local changes before pull

--- bak/server/routes/test.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-// Health check/test endpoint
-router.get('/api/test', (req, res) => {
-  res.json({ message: 'Backend is running!', time: new Date().toISOString() });
-});
-
-module.exports = router;
