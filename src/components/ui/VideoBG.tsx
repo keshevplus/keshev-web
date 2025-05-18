d90fa0a7197bd9c3f8115258ee18a0cfbbe908ab
keshevplus <keshevplus@users.noreply.github.com>
2025-05-18 11:05:53 +0300

    Update: dist/assets/browser-ponyfill-CNSGUJrq.js;Update: dist/assets/browser-ponyfill-CNSGUJrq.js.map Update: dist/assets/images/bgvideo.mp4;Update: dist/assets/images/bgvideogif.gif Update: dist/assets/images/wheelchair-icon.svg;Update: dist/assets/index-CRwKTUF0.js Update: dist/assets/index-CRwKTUF0.js.map;Update: dist/assets/index-G9-J4-60.js Update: dist/assets/index-G9-J4-60.js.map;Update: dist/assets/index-Lkr9yn2U.css Update: dist/assets/index-WGkozBvL.css;Update: dist/index.html Update: package.json;Update: pnpm-workspace.yaml Update: public/assets/images/bgvideogif.gif;Update: src/components/Navbar.tsx Update: src/components/ui/VideoBG.tsx

---
+++ src/components/ui/VideoBG.tsx
@@ -0,0 +1,23 @@
+// The provided code defines a React functional component named `VideoBG` in TypeScript.
+import React, { ReactNode } from 'react';
+
+interface VideoBGProps {
+  children?: ReactNode;
+}
+
+const VideoBG: React.FC<VideoBGProps> = ({ children }) => {
+  return (
+    <div className="absolute inset-0 overflow-hidden z-[-1]">
+      <img
+        className="absolute inset-0 object-cover w-full h-full opacity-70 pointer-events-none"
+        src="/assets/images/bgvideogif.gif"        
+        />
+       
+    
+      <div className="absolute inset-0 bg-orange-400 opacity-10"></div>
+      {children}
+    </div>
+  );
+};
+
+export default VideoBG;
