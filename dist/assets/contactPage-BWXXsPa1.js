9a35fdd224b3a15f5ae90af93369126889dd7f67
keshevplus <keshevplus@users.noreply.github.com>
2025-05-18 11:23:38 +0300

    Update: dist/_redirects;Update: dist/assets/aboutPage-JyU8Dg2N.js Update: dist/assets/aboutPage-JyU8Dg2N.js.map;Update: dist/assets/adhdPage-BBcbhT6K.js Update: dist/assets/adhdPage-BBcbhT6K.js.map;Update: dist/assets/browser-ponyfill-CNSGUJrq.js Update: dist/assets/browser-ponyfill-CNSGUJrq.js.map;Update: dist/assets/contactPage-BWXXsPa1.js Update: dist/assets/contactPage-BWXXsPa1.js.map;Update: dist/assets/diagnosisPage-BjSJ6KqV.js Update: dist/assets/diagnosisPage-BjSJ6KqV.js.map;Update: dist/assets/forms/scared_self_form.docx Update: dist/assets/forms/scared_self_form.pdf;Update: dist/assets/forms/vanderbilt_parent_form.docx Update: dist/assets/forms/vanderbilt_parent_form.pdf;Update: dist/assets/forms/vanderbilt_self_form.docx Update: dist/assets/forms/vanderbilt_self_form.pdf;Update: dist/assets/forms/vanderbilt_teacher_form.docx Update: dist/assets/forms/vanderbilt_teacher_form.pdf;Update: dist/assets/formsPage-Bc-gC6C5.js Update: dist/assets/formsPage-Bc-gC6C5.js.map;Update: dist/assets/homePage-iHNOSAKl.js Update: dist/assets/homePage-iHNOSAKl.js.map;Update: dist/assets/images/PDFicon.svg Update: dist/assets/images/accessibility-icon.svg;Update: dist/assets/images/bgvideogif.gif Update: dist/assets/images/doctor-hero.png;Update: dist/assets/images/doctor.png Update: dist/assets/images/favicon.ico;Update: dist/assets/images/favicon.png Update: dist/assets/images/greenphone.svg;Update: dist/assets/images/hero-about.jpeg Update: dist/assets/images/icon.png;Update: dist/assets/images/leaf.png Update: dist/assets/images/logo-square.png;Update: dist/assets/images/logo.png Update: dist/assets/images/main-logo-gold.png;Update: dist/assets/images/pdficon.png Update: dist/assets/images/plus.png;Update: dist/assets/images/wheelchair-icon.svg Update: dist/assets/images/wordicon.png;Update: dist/assets/images/wordicon.svg Update: dist/assets/index-CRwKTUF0.js;Update: dist/assets/index-CRwKTUF0.js.map Update: dist/assets/index-WGkozBvL.css;Update: dist/assets/servicesPage-Ag0JjB7c.js Update: dist/assets/servicesPage-Ag0JjB7c.js.map;Update: dist/contact-details.json Update: dist/index.html;Update: dist/locales/en/accessibility.json Update: dist/locales/en/common.json;Update: dist/locales/en/forms.json Update: dist/locales/en/home.json;Update: dist/locales/en/navigation.json Update: dist/locales/he/accessibility.json;Update: dist/locales/he/common.json Update: dist/locales/he/forms.json;Update: dist/locales/he/home.json Update: dist/locales/he/navigation.json;Update: dist/manifest.json Update: src/components/Navbar.tsx

--- dist/assets/contactPage-BWXXsPa1.js
+++
@@ -1,2 +0,0 @@
-const i={heading:"יצירת קשר",subheading:"השאירו פרטים ואחד מנציגנו יחזור אליכם בהקדם",body:[{title:"כתובת",description:"רמת גן, ישראל",image:"/assets/images/icon.png",icon:"🏠"},{title:"טלפון",description:"054-4777469",image:"/assets/images/icon.png",icon:"📞"},{title:"אימייל",description:"dr@keshevplus.co.il",image:"/assets/images/icon.png",icon:"✉️"}]};export{i as default};
-//# sourceMappingURL=contactPage-BWXXsPa1.js.map
