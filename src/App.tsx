7bf5f6cbe2f7beb5106007b5f892966028d248cc
keshevplus <pluskeshev@gmail.com>
2025-05-19 13:24:34 +0300

    run it

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import Footer from './components/Footer';
 
 // pages routes
 import Home from './pages/Home';
