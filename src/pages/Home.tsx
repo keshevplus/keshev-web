e803fb836d7981f6b8b33f1a1bdd86a3e43827f3
keshevplus <pluskeshev@gmail.com>
2025-05-26 09:30:21 +0300

    Backend: Automated build, commit, push, and deploy [bacpd]

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -38,7 +38,7 @@ export default function Home() {
           <div className="flex flex-col md:flex-row items-center justify-between gap-4 sm:gap-6 md:gap-8 lg:gap-12">
             {/* Hero Text Section */}
             <div className="xl:w-full md:w-2/7 order-1 px-2 sm:px-4 flex flex-col items-center justify-center">
-            <h1 className="lg:text-5xl md:text-4xl sm:text-3xl font-bold text-green-800 mb-4 md:whitespace-nowrap text-center">
+            <h1 className="lg:text-5xl md:text-4xl sm:text-3xl font-bold text-green-800 mb-4 whitespace-pre-line text-center">
                 {pageData.heading || 'ברוכים הבאים למרפאת "קשב פלוס"'}
               </h1>
                <img 
