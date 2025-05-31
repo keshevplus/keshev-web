8c7e1f055c8c7da4b21d3c426547594f7aecc98c
keshevplus <pluskeshev@gmail.com>
2025-05-31 23:18:09 +0300

    Backend: Automated build, commit, push, and deploy [bacpd]

--- src/components/FloatingPhoneNumber.tsx
+++ src/components/FloatingPhoneNumber.tsx
@@ -2,12 +2,11 @@ import React from 'react';
 
 const FloatingPhoneNumber: React.FC = () => {
   return (
-    <div className="relative inline-block">  
-      <span className="absolute hidden md:block left-0 -bottom-8 transform z-30 text-white whitespace-nowrap bg-transparent px-2 py-1 hover:text-green-100 transition-all duration-300 text-base font-semibold shine-text">
+    <div className="inline-block">
+      <span className="hidden md:inline-block ml-2 text-green-600 font-semibold whitespace-nowrap">
         055-27-399-27
       </span>
     </div>
-
   );
 };
 
